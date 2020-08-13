f62b0da58ea3df345694d35ec4daf7fbe8d791f6
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2020-08-13 14:29:47 -0500

    components proofile education, experience

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -6,11 +6,13 @@ import Spinner from '../layout/Spinner';
 import { getProfileByID } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 
 const Profile = ({
   getProfileByID,
   match,
-  profile: { profile, loading },
+  profile: { profile, loading, experience },
   auth
 }) => {
   useEffect(() => {
@@ -33,10 +35,35 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
-            <div class="profile-grid my-1">
-              <ProfileTop profile={profile} />
-              <ProfileAbout profile={profile} />
+          <div className='profile-grid my-1'>
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map(exp => (
+                    <ProfileExperience key={exp._id} experience={exp} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
             </div>
+
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(edu => (
+                    <ProfileEducation key={edu._id} education={edu} />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
+          </div>
         </Fragment>
       )}
     </Fragment>
